d6f4e8ca95dfa98eb66356dff36c74affb19db9e
Kashif Nazir <knazir@stanford.edu>
2015-12-23 01:51:38 +0300

    Added client app. Started GeneticAlgorithm class, not finished

--- algorithm/GeneticAlgorithm.java
+++ algorithm/GeneticAlgorithm.java
@@ -9,5 +9,23 @@ public class GeneticAlgorithm implements GAConstants {
 	public GeneticAlgorithm() {
 		this.randomGenerator = new Random();
 	}
+	
+	public Population evolvePopulation(Population population) {
+		Population newPopulation = new Population(population.size());
+		
+		for (int i = 0; i < population.size(); i++) {
+			Individual firstIndividual = randomSelection(population);
+			Individual secondIndividual = randomSelection(population);
+			Individual newIndividual = crossOver(firstIndividual, secondIndividual);
+			newPopulation.saveIndividual(i, newIndividual);
+		}
+		
+		for (int i = 0; i < newPopulation.size(); i++)
+			mutate(newPopulation.getIndividual(i));
+		
+		return newPopulation;
+	}
+	
+	
 
 }
